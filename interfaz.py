d895f821c168a0c0808c6ee4a678f932e14b603c
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-06-17 08:01:45 +0100

    Añadimos css a los botones

--- interfaz.py
+++ interfaz.py
@@ -18,5 +18,37 @@ class MainMenu(App):
             id="menu",
         )
 
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        button_id = event.button.id
+        if button_id == "alumno":
+            self.show_alumno_screen()
+
+    def show_alumno_screen(self):
+        self.clear_screen()
+        self.mount(AlumnoScreen())
+
+    def clear_screen(self):
+        self.query_one("#menu").remove()
+
+class AlumnoScreen(Container):
+    CSS = """
+    #alumno_menu {
+        align: center middle;
+        height: 100%;
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        yield Container(
+            Button("Añadir", id="add"),
+            Button("Editar", id="edit"),
+            Button("Borrar", id="delete"),
+            id="alumno_menu",
+        )
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        button_id = event.button.id
+        print(f"{button_id} button pressed")
+
 if __name__ == "__main__":
     MainMenu().run()
