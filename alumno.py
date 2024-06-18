e0860222440b65fcf3241e00a0b1a8ded00ff3bb
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-06-18 08:39:00 +0100

    Añadimos los CRUD

---
+++ alumno.py
@@ -0,0 +1,17 @@
+class Nota:
+    def __init__(self, nombre:str) -> None:
+        self.nombre = nombre
+
+    def read(self):
+        return self.id + ", " + self.nombre
+
+    def __str__(self) -> str:
+        return self.nombre
+
+    def update(self, idNuevo:int, nombreNuevo:str):
+        self.id = idNuevo
+        self.nombre = nombreNuevo
+
+    def delete(self):
+        self.id = None
+        self.nombre = None
